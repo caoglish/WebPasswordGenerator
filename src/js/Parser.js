14814cb2b9fa063aa810e4b6bf3393fbabbc8287
caoglish <caoglish.sina@gmail.com>
2018-02-01 16:05:12 +1100

    refactor parser

--- src/js/Parser.js
+++ src/js/Parser.js
@@ -8,16 +8,14 @@ function block(text,type){
 }
 function parse(password) {
     let blocklist=[];
-
-    let regex_letters=/^[a-zA-Z]+/;
-    let regex_numbers=/^[0-9]+/;
-    let regex_symbols=/^[^a-zA-Z0-9]+/;
-    list={letter:regex_letters,number:regex_numbers,symbol:regex_symbols};
+    let regexList={letter:/^[a-zA-Z]+/,
+        number:/^[0-9]+/,
+        symbol:/^[^a-zA-Z0-9]+/};
     let matches;
 
     while(password.length>0){
-        for (type in list){
-            regex = list[type];
+        for (type in regexList){
+            regex = regexList[type];
             matches = password.match(regex);
                     if(!_.isEmpty(matches)){
                         password = _.trimStart(password,matches[0]);  //console.log(matches[0]);
