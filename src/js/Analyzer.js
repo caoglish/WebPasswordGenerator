fc8509840aebff381af7e83c4b0e75e088b1d15d
caoglish <caoglish.sina@gmail.com>
2018-02-06 15:30:02 +1100

    initialize function analyseRepetitive

--- src/js/Analyzer.js
+++ src/js/Analyzer.js
@@ -1,12 +1,11 @@
 var _ = require('lodash');
 var parser = require('./Parser');
 
-function initTypeList()
-{
+function initTypeList() {
     return {
-        letter:0,
-        number:0,
-        symbol:0
+        letter: 0,
+        number: 0,
+        symbol: 0
     }
 }
 
@@ -19,31 +18,66 @@ function countBlockTypeNumber(parsedData) {
     return blockNumber;
 }
 
-function countTypeSize(parsedData){
+function countTypeSize(parsedData) {
     let list = initTypeList();
     for (block of parsedData) {
-        
+
         if (!_.isNumber(list[block.type])) list[block.type] = block.block.length;
-        else list[block.type]+= block.block.length;
+        else list[block.type] += block.block.length;
     }
     return list;
 }
 
-function countletterCaseSize(parsedData){
-    let list = {upperCase:0,lowerCase:0};
+function countletterCaseSize(parsedData) {
+    let list = {
+        upperCase: 0,
+        lowerCase: 0
+    };
     for (block of parsedData) {
         if (block.type !== 'letter') continue;
-        let charList=block.block.split("");
-        charList.forEach(function(item){
-            if(item === item.toLowerCase()) list.lowerCase++;
-            if(item === item.toUpperCase()) list.upperCase++;
+        let charList = block.block.split("");
+        charList.forEach(function (item) {
+            if (item === item.toLowerCase()) list.lowerCase++;
+            if (item === item.toUpperCase()) list.upperCase++;
         });
     }
     return list;
 }
 
-function analyseOnlyType(parsedData){
-    return parsedData.length === 1? parsedData[0].type:null;
+function analyseOnlyType(parsedData) {
+    return parsedData.length === 1 ? parsedData[0].type : null;
+}
+
+
+function analyseRepetitive(password) {
+    let len = password.length;
+    let ct = parseInt(len / 2); //check times
+    let list = [];
+
+    for (let cti = 2; cti <= ct; cti++) {
+        for (let TargetIndex = 0; TargetIndex <= len - cti * 2; TargetIndex++) {
+            let targetStr = password.substr(TargetIndex, cti);
+            for (let scanStart = TargetIndex + cti; scanStart <= len - cti; scanStart++) {
+                let scanStr = password.substr(scanStart, cti);
+                if (scanStr === targetStr) {
+                    list.push({
+                        str: scanStr,
+                        Begin: TargetIndex,
+                        End: TargetIndex + cti
+
+                    });
+                }
+            }
+        }
+
+    }
+
+    return list;
+
+
+
+
+
 }
 
 
@@ -54,11 +88,12 @@ function analyse(password) {
         size: password.length
     };
 
-    analyzedData.typeSize =  countTypeSize(analyzedData.parsedData);//size of each type
-    analyzedData.blockNumber = analyzedData.parsedData.length;//number of blocks
-    analyzedData.blockTypeNumber = countBlockTypeNumber(analyzedData.parsedData);//number of each type
-    analyzedData.letterCaseSize = countletterCaseSize(analyzedData.parsedData);//number of each type of letter case
-    analyzedData.onlyType = analyseOnlyType(analyzedData.parsedData);//number of each type of letter case
+    analyzedData.typeSize = countTypeSize(analyzedData.parsedData); //size of each type
+    analyzedData.blockNumber = analyzedData.parsedData.length; //number of blocks
+    analyzedData.blockTypeNumber = countBlockTypeNumber(analyzedData.parsedData); //number of each type
+    analyzedData.letterCaseSize = countletterCaseSize(analyzedData.parsedData); //number of each type of letter case
+    analyzedData.onlyType = analyseOnlyType(analyzedData.parsedData); //number of each type of letter case
+    analyzedData.repetitive = analyseRepetitive(analyzedData.password); //number of each type of letter case
 
     return analyzedData;
 }
--- test/testAnalyzer.js
+++ test/testAnalyzer.js
@@ -5,12 +5,12 @@ var analyzer = require("../src/js/Analyzer");
 describe('Analyzer', function() {
 	describe('Analyzer', function() {
 		it('Analysing', function() {
-            console.log(analyzer.analyse("abc###233fasdf3E#@$f"))
-
+            //console.log(analyzer.analyse("abc###233fasdf3E#@$f"))
+            console.log(analyzer.analyse("cdcdcdcdcdcd"))
         });
 
         it('Analysing only type', function() {
-            console.log(analyzer.analyse("0123456789"))
+            //console.log(analyzer.analyse("0123456789"))
 
         });
     });
